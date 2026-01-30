c1c71ba528ad6e1212ca2fe2b3bca6013ae9417c
mateusz_i <255603@edu.p.lodz.pl>
2026-01-30 09:30:22 +0100

    01 Zmiana metody Policz

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int Policz(int a, int b) {
 int main() {
 
     std::cout << "Hello and welcome to " << "!\n";
-
     std::cout << "w pierwszym branchu 2+3 =  " << Policz(2,3) << std::endl;
 
     return 0;
